20b31b50918f354f2eebe13b46f988e167f9d2e1
Vikas Movva <vikas.s.movva@gmail.com>
2022-02-21 22:09:40 -0500

    created a placeholder More widget

---
+++ lib/more.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class More extends StatelessWidget {
+  const More({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text('Favorites'),
+    );
+  }
+}
